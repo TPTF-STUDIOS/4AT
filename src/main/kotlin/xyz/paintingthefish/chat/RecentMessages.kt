3167c1c5ec44b8e9331e3ca3b1d69b6fae51904e
max <mlhader@outlook.com>
2026-08-29 22:03:17 -0400

    Update RecentMessages.kt in order to have more robust size-checking and null-checking qualities

--- src/main/kotlin/xyz/paintingthefish/chat/RecentMessages.kt
+++ src/main/kotlin/xyz/paintingthefish/chat/RecentMessages.kt
@@ -1,4 +1,4 @@
-package xyz.paintingthefish.chatti
+package xyz.paintingthefish.chat
 
 import java.nio.ByteBuffer
 import java.util.*
@@ -7,18 +7,31 @@ class RecentMessages {
     constructor(maxSize: Int) {
         maxBufferSize = maxSize
     }
-
     constructor()
 
-    private var maxBufferSize = 0
+    enum class MessageBufferErrors {
+        OK,
+        TOO_BIG,
+        TOO_SMALL,
+    }
+
+    private var maxBufferSize: Int = 1 // to be overridden by constructor
     private val messages: Queue<ByteArray> = ArrayDeque<ByteArray>(maxBufferSize)
 
     @Synchronized
-    fun add(msg: ByteArray?) {
+    fun add(msg: ByteArray): MessageBufferErrors {
+        if (msg.size < 2060) {
+            System.err.println("Message size is too short! :(")
+            return MessageBufferErrors.TOO_SMALL
+        } else if (msg.size > 3000) {
+            System.err.println("Message size is too long! :(")
+            return MessageBufferErrors.TOO_BIG
+        }
         if (messages.size >= maxBufferSize) {
             messages.poll() // Discard the oldest message
         }
         messages.offer(msg) // Add the newest message
+        return MessageBufferErrors.OK
     }
 
     fun getId(id: Int): ByteArray? {
